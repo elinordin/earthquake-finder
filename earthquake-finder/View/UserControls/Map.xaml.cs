1da2ccc0de778c1a52c254a659ae2ed4e9ee6fd8
elinordin <elin.evelina@live.se>
2024-02-11 19:30:18 +0100

    add map from mapsui

---
+++ earthquake-finder/View/UserControls/Map.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace earthquake_finder.View.UserControls
+{
+    public partial class Map : UserControl
+    {
+        public Map()
+        {
+            InitializeComponent();
+
+            var mapControl = new Mapsui.UI.Wpf.MapControl();
+            mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+
+            Content = mapControl;
+        }
+    }
+}
