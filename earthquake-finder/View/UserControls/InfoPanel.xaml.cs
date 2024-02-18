d8063b9d6f956d59fe55ba15504ebb33dfc58a2d
elinordin <elin.evelina@live.se>
2024-02-18 17:44:51 +0100

    Clean up unused imports

--- earthquake-finder/View/UserControls/InfoPanel.xaml.cs
+++ earthquake-finder/View/UserControls/InfoPanel.xaml.cs
@@ -1,23 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace earthquake_finder.View.UserControls
 {
-    /// <summary>
-    /// Interaction logic for InfoPanel.xaml
-    /// </summary>
     public partial class InfoPanel : UserControl
     {
         public InfoPanel()
