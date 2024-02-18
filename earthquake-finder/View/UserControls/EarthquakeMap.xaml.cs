7bbe1fc729e31424a33d4434925e8096daa88aba
elinordin <elin.evelina@live.se>
2024-02-18 18:05:23 +0100

    Move map methods to its own class

--- earthquake-finder/View/UserControls/EarthquakeMap.xaml.cs
+++ earthquake-finder/View/UserControls/EarthquakeMap.xaml.cs
@@ -11,40 +11,18 @@ using System.Windows.Controls;
 
 namespace earthquake_finder.View.UserControls
 {
-    public partial class EarthquakeMap : UserControl
+    public class MapManager
     {
-        public EarthquakeMap()
-        {
-            InitializeComponent();
-            DataContext = Global.Instance;
-            CreateMapAsync();
-
-            Global.Instance.PropertyChanged += HandleGlobalPropertyChange;
-        }
-
-        private void HandleGlobalPropertyChange(object sender, PropertyChangedEventArgs e)
-        {
-            var pointsLayer = mapsuiMapControl.Map.Layers.FirstOrDefault(layer => layer.Name == "Points");
-
-            if(pointsLayer != null)
-            {
-                mapsuiMapControl.Map.Layers.Remove(pointsLayer);
-            }
-
-            mapsuiMapControl.Map.Layers.Add(CreatePointsLayer());
-        }
-
-        private Task<Map> CreateMapAsync()
+        public static Map CreateMapContent()
         {
             var map = new Map();
             map.Layers.Add(OpenStreetMap.CreateTileLayer()); // Base layer
             map.Layers.Add(CreatePointsLayer());
-            mapsuiMapControl.Map = map;
-            mapsuiMapControl.Map.Navigator.Limiter = new ViewportLimiterKeepWithinExtent();
-            return Task.FromResult(map);
+            map.Navigator.Limiter = new ViewportLimiterKeepWithinExtent();
+            return map;
         }
 
-        private static ILayer CreatePointsLayer()
+        public static ILayer CreatePointsLayer()
         {
             return new MemoryLayer
             {
@@ -63,7 +41,7 @@ namespace earthquake_finder.View.UserControls
                 var point = SphericalMercator.FromLonLat(earthquake.Longitude, earthquake.Latitude).ToMPoint();
 
                 var pointFeature = new PointFeature(point)
-                { 
+                {
                     Styles = new List<IStyle>([CreatePointStyle(earthquake)])
                 };
 
@@ -91,16 +69,19 @@ namespace earthquake_finder.View.UserControls
 
             Color vectorFill;
 
-            if (earthquake.Magnitude < 4.5) 
+            if (earthquake.Magnitude < 4.5)
             {
                 vectorFill = lightBlue; // small
-            } else if (earthquake.Magnitude < 5.5) 
+            }
+            else if (earthquake.Magnitude < 5.5)
             {
                 vectorFill = lightYellow; // medium
-            } else if(earthquake.Magnitude < 6.5) 
+            }
+            else if (earthquake.Magnitude < 6.5)
             {
                 vectorFill = lightOrange; // larger
-            } else
+            }
+            else
             {
                 vectorFill = orange; // large
             }
@@ -116,6 +97,32 @@ namespace earthquake_finder.View.UserControls
                     Width = 5
                 }
             };
+        }
+    }
+
+    public partial class EarthquakeMap : UserControl
+    {
+        private readonly Map mapContent;
+        public EarthquakeMap()
+        {
+            InitializeComponent();
+            DataContext = Global.Instance;
+
+            mapContent = MapManager.CreateMapContent();
+            mapsuiMapControl.Map = mapContent;
+
+            Global.Instance.PropertyChanged += HandleGlobalPropertyChange;
+        }
+
+        private void HandleGlobalPropertyChange(object sender, PropertyChangedEventArgs e)
+        {
+            var pointsLayer = mapContent.Layers.FirstOrDefault(layer => layer.Name == "Points");
+
+            if(pointsLayer != null)
+            {
+                mapContent.Layers.Remove(pointsLayer);
+                mapContent.Layers.Add(MapManager.CreatePointsLayer());
+            }      
         }
     }
 }
